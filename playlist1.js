fae0721db129f5c8136afc9aed4b2495ce7e7427
Sahil <136996478+Sahil-3000@users.noreply.github.com>
2024-12-01 18:32:41 +0530

    Add files via upload

---
+++ playlist1.js
@@ -0,0 +1,81 @@
+const playlist1 = {
+    name: "Diljit Hits",
+    songs: [
+      //Diljit Dosanjh
+    { 
+        title: "Lover      ",
+        artist: "Diljit Dosanjh     ",       
+        lyrics: "Lover",             
+        artistPhoto: "covers/Diljit Dosanjh.png",       
+        cover: "covers/lover_diljit.png",      
+        audio: "songs/lover_diljit.mp3",     
+        searchString: "diljit k gane lover  love diljit lovr lvr diljit diljeet diljjt dil jit lvr"
+    
+    },//40
+
+    { 
+        title: "Case        ", 
+        artist: "Diljit Dosanjh     ", 
+        lyrics: "Case",       
+        artistPhoto: "covers/Diljit Dosanjh.png", 
+        cover: "covers/case_diljit.png", 
+        audio: "songs/case_diljit.mp3", 
+        searchString: "diljit k gane diljit diljeet diljjt dil jitdosanjh doshanjh dosnjh " 
+    },//41
+    { 
+        title: "Ishq Di Baajiyaan        ", 
+        artist: "Diljit Dosanjh     ", 
+        lyrics: "Ishq Di Baajiyaan",         
+        artistPhoto: "covers/Diljit Dosanjh.png", 
+        cover: "covers/ishq di baajiyaan_diljit.png", 
+        audio: "songs/ishq di baajiyaan_diljit.mp3", 
+        searchString: "diljit k gane diljit diljeet diljjt dil jit " 
+    },//42
+    { 
+        title: "Love Ya        ", 
+        artist: "Diljit Dosanjh     ",  
+        lyrics:"Love Ya",        
+        artistPhoto: "covers/Diljit Dosanjh.png", 
+        cover: "covers/lova ya_diljit.png", 
+        audio: "songs/love ya_diljit.mp3", 
+        searchString: "diljit k gane diljit diljeet diljjt dil jit " 
+    },//43
+    { 
+        title: "Lalkara         ", 
+        artist: "Diljit Dosanjh     ", 
+        lyrics:"Lalkara",        
+        artistPhoto: "covers/Diljit Dosanjh.png", 
+        cover: "covers/lalkara_diljit.png", 
+        audio: "songs/lalkara_diljit.mp3", 
+        searchString: "diljit k gane diljit diljeet diljjt dil jit " 
+    },//44
+    { 
+        title: "Tu Kya Jaane        ", 
+        artist: "Yashika Sikka, Diljit", 
+        lyrics:"Tu Kya Jaane",        
+        artistPhoto: "covers/Diljit Dosanjh.png", 
+        cover: "covers/tu kya jaane_diljit.png", 
+        audio: "songs/tu kya jaane_diljit.mp3", 
+        searchString: "diljit k gane diljit diljeet diljjt dil jit " 
+    },//45
+    { 
+        title: "Bol Mohabbat        ", 
+        artist: "AR Rehman, Kailash Kher,Diljit", 
+        lyrics:"Bol Mohabbat",        
+        artistPhoto: "covers/Diljit Dosanjh.png", 
+        cover: "covers/bol mohabbat_diljit.png", 
+        audio: "songs/bol mohabbat_diljit.mp3", 
+        searchString: "diljit k gane diljit diljeet diljjt dil jit " 
+    },//46
+    { 
+        title: "Vida Karo        ", 
+        artist: "Arijit Singh,Diljjt", 
+        lyrics:"Vida Karo",        
+        artistPhoto: "covers/Diljit Dosanjh.png", 
+        cover: "covers/vida karo_diljit.png", 
+        audio: "songs/vida karo_diljit.mp3", 
+        searchString: "diljit k gane diljit diljeet diljjt dil jit " 
+    },//47
+    ],
+  };
+  
---
+++ playlist2.js
@@ -0,0 +1,62 @@
+const playlist2 = {
+    name: "Arjan Hits",
+    songs: [
+        
+      //Arjan Dhillon-Glorious and more
+    { 
+        title: "Youth Flow      ", 
+        artist: "Arjan Dhillon    ",
+        lyrics:"Youth Flow",        
+        artistPhoto: "covers/Arjan Dhillon.png", 
+        cover: "covers/Chobbar.jpg", 
+        audio: "songs/youth flow.mp3", 
+        searchString: "arjan songs arjan jawani song arjan"
+    },//song4
+    { 
+        title: "Glorious      ", 
+        artist: "Arjan Dhillon    ",
+        lyrics:"Glorious",        
+        artistPhoto: "covers/Arjan Dhillon.png", 
+        cover: "covers/Chobbar.jpg", 
+        audio: "songs/glorious.mp3", 
+        searchString: "arjan songs glorious glowrious glorius gloris"
+    },//song5
+    { 
+        title: "Maavan      ", 
+        artist: "Arjan Dhillon    ",
+        lyrics:"Maavan",        
+        artistPhoto: "covers/Arjan Dhillon.png", 
+        cover: "covers/Chobbar.jpg", 
+        audio: "songs/maavan.mp3", 
+        searchString: "arjan songs mava maanvan mava maava maa maa song arjan"
+    },//song6
+    { 
+        title: "Back To Sikhi      ", 
+        artist: "Arjan Dhillon    ",
+        lyrics:"Back To Sikhi",        
+        artistPhoto: "covers/Arjan Dhillon.png", 
+        cover: "covers/Chobbar.jpg", 
+        audio: "songs/back to sikhi.mp3", 
+        searchString: "arjan songs pagg back to siki sikhi sikki"
+    },//song7
+    { 
+        title: "Biography      ", 
+        artist: "Arjan Dhillon    ",
+        lyrics:"Biography",        
+        artistPhoto: "covers/Arjan Dhillon.png", 
+        cover: "covers/Chobbar.jpg", 
+        audio: "songs/biography.mp3", 
+        searchString: "arjan songs bigraphy bigrafi biographi "
+    },//song8
+    { 
+        title: "Tyar      ", 
+        artist: "Arjan Dhillon    ",
+        lyrics:"Tyar",        
+        artistPhoto: "covers/Arjan Dhillon.png", 
+        cover: "covers/Chobbar.jpg", 
+        audio: "songs/tyar.mp3", 
+        searchString: "arjan songs tyaar tyar ready tyar hi ni hoye"
+    },//song9
+    ],
+  };
+  
